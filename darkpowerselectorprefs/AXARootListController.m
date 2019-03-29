b3b1f164009f8db37581f43d7daa8a062d2b90bb
Shreyas Lad <shadowtemplates@gmail.com>
2019-03-29 09:11:21 -0700

    Update AXARootListController.m

--- darkpowerselectorprefs/AXARootListController.m
+++ darkpowerselectorprefs/AXARootListController.m
@@ -1,5 +1,48 @@
 #include "AXARootListController.h"
 
+#define kTintColor [UIColor colorWithRed:0.09 green:0.12 blue:0.16 alpha:1.0]
+
+@interface DARKHeader : UITableViewCell {
+  UILabel *label;
+  UILabel *underLabel;
+}
+@end
+
+@implementation DARKHeader
+- (id)initWithSpecifier:(PSSpecifier *)specifier {
+  self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+  if (self) {
+		#define kWidth [[UIApplication sharedApplication] keyWindow].frame.size.width
+		CGRect labelFrame = CGRectMake(0, -15, kWidth, 80);
+		CGRect underLabelFrame = CGRectMake(0, 35, kWidth, 60);
+
+		label = [[UILabel alloc] initWithFrame:labelFrame];
+		[label setNumberOfLines:1];
+		label.font = [UIFont systemFontOfSize:50];
+		[label setText:@"DarkPowerSelector"];
+		//[label setBackgroundColor:[UIColor clearColor]];
+		label.textColor = kTintColor;
+		label.textAlignment = NSTextAlignmentCenter;
+
+		underLabel = [[UILabel alloc] initWithFrame:underLabelFrame];
+		[underLabel setNumberOfLines:1];
+		underLabel.font = [UIFont systemFontOfSize:20];
+		[underLabel setText:@"yes"];
+		//[underLabel setBackgroundColor:[UIColor clearColor]];
+		underLabel.textColor = [UIColor grayColor];
+		underLabel.textAlignment = NSTextAlignmentCenter;
+
+		[self addSubview:label];
+		[self addSubview:underLabel];
+  }
+  return self;
+}
+- (CGFloat)preferredHeightForWidth:(CGFloat)arg1 {
+  CGFloat prefHeight = 75.0;
+  return prefHeight;
+}
+@end
+
 @implementation AXARootListController
 
 - (NSArray *)specifiers {
